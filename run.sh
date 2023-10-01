5a7d1c32b6006b02bb308f6c071071c30cb2c450
tanpengsccd <tanpengcd@gmail.com>
2023-10-01 12:25:33 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
-# for Sonoma for sudo 
+# 0. for Sonoma
+# 1. Boot into Recovery and run  `csrutil disable`
+# 2. downloand this this ,amd run `sh run.sh`
+
 sudo rm /var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
 
 sudo rm /var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
