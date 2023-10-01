969e6f83de73836da1afbffee4c6eae1dddc7405
谭鹏(深圳联友科技有限公司创新产品群数字营销产品中心) <tanpeng@szlanyou.com>
2023-10-01 11:15:37 +0800

    script

---
+++ run.sh
@@ -0,0 +1,8 @@
+# for Sonoma for sudo 
+sudo rm /var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
+
+sudo rm /var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
+
+sudo touch /var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
+
+sudo touch /var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
